d2399a765416eb561e73d0cd5c4c6343ceb0f843
zqhong <zqh0513@gmail.com>
2018-09-18 18:44:56 +0800

    updated: 完善安装器

--- installer.go
+++ installer.go
@@ -4,6 +4,7 @@ import (
 	"archive/zip"
 	"flag"
 	"fmt"
+	"go/build"
 	"io"
 	"io/ioutil"
 	"log"
@@ -12,24 +13,22 @@ import (
 	"path/filepath"
 	"strings"
 	"time"
-	"go/build"
 )
 
 /*
 使用：
 
 $ cd $GOPATH/src/github.com/zqhong
-$ ./installer --project-name=demo
- */
+$ ./installer -project-name=demo
+*/
 func main() {
 
 	go func() {
-		fmt.Println("installing...")
-		fmt.Printf("%s", "[")
-		consoleStr := "█"
-		for i := 0; i != 10; i = i + 1 {
+		fmt.Println("Installing Albedo(A Gin API Skeleton)...")
+		consoleStr := "██"
+		for i := 0; i != 25; i = i + 1 {
 			fmt.Printf("%s", consoleStr)
-			time.Sleep(time.Second)
+			time.Sleep(time.Millisecond * 250)
 		}
 	}()
 
@@ -51,15 +50,16 @@ func main() {
 	files, _ := ioutil.ReadDir("./tmp")
 
 	var projectName string
-	flag.StringVar(&projectName, "project-projectName", "albedo", "project projectName")
+	flag.StringVar(&projectName, "project-name", "albedo", "project name")
 	flag.Parse()
 
 	os.Rename("./tmp/"+files[0].Name(), "./"+files[0].Name())
 	os.Remove("tmp")
 	os.Remove("tmp.zip")
 	os.Rename("./"+files[0].Name(), "./"+projectName)
-
 	renameProject("./"+projectName, projectName)
+
+	fmt.Printf("%s", "\nInstall Succeeded\n\n")
 }
 
 func unzipDir(zipFile, dir string) {
@@ -96,6 +96,8 @@ func unzipDir(zipFile, dir string) {
 }
 
 func renameProject(fileDir string, projectName string) {
+	// projectName 示例：demo
+	// pkgName 示例：github.com/zqhong
 	projectName = strings.TrimSuffix(projectName, "/")
 	projectName = strings.TrimPrefix(projectName, "/")
 
@@ -103,10 +105,11 @@ func renameProject(fileDir string, projectName string) {
 	if goPath == "" {
 		goPath = build.Default.GOPATH
 	}
-	pwdDir, _ := os.Getwd()
-
-	fmt.Println("go path: " + goPath)
-	fmt.Println("pwd path: " + pwdDir)
+	pwdPath, _ := os.Getwd()
+	pkgName := pwdPath
+	pkgName = strings.Replace(pkgName, goPath+"/src/", "", 1)
+	pkgName = strings.TrimSuffix(pkgName, "/")
+	pkgName = strings.TrimPrefix(pkgName, "/")
 
 	files, _ := ioutil.ReadDir(fileDir)
 	for _, file := range files {
@@ -118,7 +121,7 @@ func renameProject(fileDir string, projectName string) {
 			content := string(buf)
 
 			//替换
-			newContent := strings.Replace(content, "github.com/zqhong/albedo/", projectName+"/", -1)
+			newContent := strings.Replace(content, "github.com/zqhong/albedo/", pkgName+"/"+projectName+"/", -1)
 
 			//重新写入
 			ioutil.WriteFile(path, []byte(newContent), 0)
